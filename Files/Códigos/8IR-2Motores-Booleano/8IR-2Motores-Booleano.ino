3204055b75e498e8cd84426b9b55ceebf45eb900
jvfd3 <joaovitorfd2000@gmail.com>
2023-11-13 15:41:10 -0300

    renaming

--- Files/Códigos/8IR-2Motores-Booleano/8IR-2Motores-Booleano.ino
+++ Files/Códigos/8IR-2Motores-Booleano/8IR-2Motores-Booleano.ino
@@ -1,13 +1,15 @@
-// Defina os pinos dos sensores
+// Defina os pinos dos sensores - Hys301
 int pinosSensores[] = {A0, A1, A2, A3, A4, A5, A6, A7};
 
 // Defina os pinos dos motores
 int motorEsquerdo = 2;
 int motorDireito = 3;
 
-void setup() {
+void setup()
+{
   // Inicializa os pinos dos sensores como entradas
-  for (int i = 0; i < 8; i++) {
+  for (int i = 0; i < 8; i++)
+  {
     pinMode(pinosSensores[i], INPUT);
   }
 
@@ -16,29 +18,38 @@ void setup() {
   pinMode(motorDireito, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   // Lê os valores dos sensores
   int valoresSensores[8];
-  for (int i = 0; i < 8; i++) {
+  for (int i = 0; i < 8; i++)
+  {
     valoresSensores[i] = digitalRead(pinosSensores[i]);
   }
 
   // Lógica de seguir a linha
   int velocidadeMotor = 100;
 
-  if (valoresSensores[0] && valoresSensores[1] && valoresSensores[2] && valoresSensores[3] && !valoresSensores[4] && !valoresSensores[5] && !valoresSensores[6] && !valoresSensores[7]) {
+  if (valoresSensores[0] && valoresSensores[1] && valoresSensores[2] && valoresSensores[3] && !valoresSensores[4] && !valoresSensores[5] && !valoresSensores[6] && !valoresSensores[7])
+  {
     // Mover para frente
     analogWrite(motorEsquerdo, velocidadeMotor);
     analogWrite(motorDireito, velocidadeMotor);
-  } else if (!valoresSensores[0] && !valoresSensores[1] && !valoresSensores[2] && !valoresSensores[3] && valoresSensores[4] && valoresSensores[5] && valoresSensores[6] && valoresSensores[7]) {
+  }
+  else if (!valoresSensores[0] && !valoresSensores[1] && !valoresSensores[2] && !valoresSensores[3] && valoresSensores[4] && valoresSensores[5] && valoresSensores[6] && valoresSensores[7])
+  {
     // Mover para trás
     analogWrite(motorEsquerdo, -velocidadeMotor);
     analogWrite(motorDireito, -velocidadeMotor);
-  } else if (!valoresSensores[0] && !valoresSensores[1] && valoresSensores[2] && valoresSensores[3] && !valoresSensores[4] && !valoresSensores[5] && !valoresSensores[6] && !valoresSensores[7]) {
+  }
+  else if (!valoresSensores[0] && !valoresSensores[1] && valoresSensores[2] && valoresSensores[3] && !valoresSensores[4] && !valoresSensores[5] && !valoresSensores[6] && !valoresSensores[7])
+  {
     // Virar à esquerda
     analogWrite(motorEsquerdo, -velocidadeMotor);
     analogWrite(motorDireito, velocidadeMotor);
-  } else if (!valoresSensores[0] && !valoresSensores[1] && !valoresSensores[2] && !valoresSensores[3] && valoresSensores[4] && valoresSensores[5] && !valoresSensores[6] && !valoresSensores[7]) {
+  }
+  else if (!valoresSensores[0] && !valoresSensores[1] && !valoresSensores[2] && !valoresSensores[3] && valoresSensores[4] && valoresSensores[5] && !valoresSensores[6] && !valoresSensores[7])
+  {
     // Virar à direita
     analogWrite(motorEsquerdo, velocidadeMotor);
     analogWrite(motorDireito, -velocidadeMotor);
