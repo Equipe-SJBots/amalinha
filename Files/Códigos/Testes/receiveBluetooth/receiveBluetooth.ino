0f67c3ca411d3fb08473c3bde4c1b742e13847ff
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2023-11-18 14:58:39 -0300

    Receive Bluetooth

---
+++ Files/Códigos/Testes/receiveBluetooth/receiveBluetooth.ino
@@ -0,0 +1,30 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(5, 6); // (TX, RX) - HC05 (T-Transmits; R-Receives) - BT-Transmits, Arduino_Receives
+
+String informacaoRecebida = "";
+char c;
+
+void setup() {
+    mySerial.begin(9600);
+    Serial.begin(9600);
+}
+
+int getValorBT() {
+    while (mySerial.available() > 0) {
+        c = mySerial.read();
+        if (c == '\n') {
+            Serial.print(informacaoRecebida);
+            informacaoRecebida = "";
+        } else {
+            informacaoRecebida += c;
+        }
+    }
+    return informacaoRecebida.toInt();
+}
+
+void loop() {
+    myValue = getValorBT();
+    coeficiente = myValue % 10000;
+    valor = myValue - coeficiente;-10000;
+    Serial.print("(Coeficiente: Valor)");
+}
